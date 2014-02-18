3b0a6ab7bd7aae63c726eb2940b405d8071078a9
Jan Andersson <jan.andersson@gmail.com>
2014-02-18 21:45:14 +0100

    Redirect to HTTPS unless localhost

--- server.js
+++ server.js
@@ -5,17 +5,28 @@ var io      = require("socket.io");         // web socket external module
 var easyrtc = require("easyrtc");           // EasyRTC external module
 
 // Setup and configure Express http server. Expect a subfolder called "static" to be the web root.
-var httpApp = express();
-httpApp.configure(function() {
-    httpApp.use(express.static(__dirname + "/static/"));
+var app = express();
+
+function requireHTTPS(req, res, next) {
+  var secure = req.headers['x-forwarded-proto'] && req.headers['x-forwarded-proto'] === "https";
+  var local = (/^localhost/).test(req.get('host'));
+  if (!secure && !local) {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+app.use(requireHTTPS);
+
+app.configure(function() {
+  app.use(express.static(__dirname + "/static/"));
 });
 
 // Start Express http server on port 8080
 var port = Number(process.env.PORT || 8080);
-var webServer = http.createServer(httpApp).listen(port);
+var webServer = http.createServer(app).listen(port);
 
 // Start Socket.io so it attaches itself to Express server
 var socketServer = io.listen(webServer, {"log level":1});
 
 // Start EasyRTC server
-var rtc = easyrtc.listen(httpApp, socketServer);
+var rtc = easyrtc.listen(app, socketServer);
